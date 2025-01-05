8f8459ee6dd95f282dff6be2beaf00d337f603c0
Rishoban Yokanathan <rishoban27@gmail.com>
2025-01-05 09:25:16 +0530

    Create network.py

---
+++ ppo/network.py
@@ -0,0 +1,101 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+
+class CriticNetwork(nn.Module):
+    def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_actions, name):
+        super(CriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_action = n_actions
+        self.name = name
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+
+        self.bn1 = nn.LayerNorm(self.fc1_dims)
+        self.bn2 = nn.LayerNorm(self.fc2_dims)
+
+        self.action_value = nn.Linear(self.n_action, self.fc2_dims)
+
+        self.q = nn.Linear(self.fc2_dims, 1)
+
+        f1 = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f2 = 1 / np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        f3 = 0.003
+        self.q.weight.data.uniform_(-f3, f3)
+        self.q.bias.data.uniform_(-f3, f3)
+
+        f4 = 1 / np.sqrt(self.action_value.weight.data.size()[0])
+        self.action_value.weight.data.uniform_(-f4, f4)
+        self.action_value.bias.data.uniform_(-f4, f4)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta, weight_decay=0.01)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        state_value = self.fc1(state)
+        state_value = self.bn1(state_value)
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state_value)
+        state_value = self.bn2(state_value)
+        state_value = F.relu(state_value)
+        state_value = self.q(state_value)
+        return state_value
+
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name):
+        super(ActorNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_action = n_actions
+        self.name = name
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+
+        self.bn1 = nn.LayerNorm(self.fc1_dims)
+        self.bn2 = nn.LayerNorm(self.fc2_dims)
+
+        self.mu = nn.Linear(self.fc2_dims, self.n_action)
+
+        f1 = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f2 = 1 / np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        f3 = 0.003
+        self.mu.weight.data.uniform_(-f3, f3)
+        self.mu.bias.data.uniform_(-f3, f3)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = T.softmax(self.mu(x), dim=-1)
+
+        return x
+
